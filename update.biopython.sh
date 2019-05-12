46cf35bdd6d6d646f4ecfd6aa0e138ac70566cba
Duo Peng <pengxunduo@gmail.com>
2019-05-12 18:17:23 -0400

    Create update.biopython.sh

---
+++ update.biopython.sh
@@ -0,0 +1,4 @@
+sudo apt install python3-pip # install pip for python3
+sudo pip3 install --upgrade pip # update pip
+pip3 install -U pip #update pip
+sudo pip3 install biopython --upgrade #update biopython
